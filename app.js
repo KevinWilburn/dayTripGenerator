d24eaeaa4bb2600a8aab3f3b2b255259647ec418
Kevin Wilburn <wilburnkevin200@gmail.com>
2021-05-11 14:17:32 -0400

    Added funtion in app.js to generate random destination, rest., trans., and entertainment.

--- app.js
+++ app.js
@@ -3,4 +3,18 @@ let restaurant=["Chiptole","Olive Garden", "Buffalo Wild Wings"];
 let transportation=["Plane","Train","Car"];
 let entertainment=["Beach day","Baseball game","Hiking"];
 
+function findRandomFromCollection(array){
+      let randomItem = array[Math.floor(Math.random() * array.length)];
+    return randomItem;
+}
+
+let randomItemFromDestination = findRandomFromCollection(destination);
+let randomItemFromRestaurant = findRandomFromCollection(restaurant);
+let randomItemFromTransportation = findRandomFromCollection(transportation);
+let randomItemFromEntertainment= findRandomFromCollection(entertainment);
+
+console.log(randomItemFromDestination);
+console.log(randomItemFromRestaurant);
+console.log(randomItemFromTransportation);
+console.log(randomItemFromEntertainment);
 
