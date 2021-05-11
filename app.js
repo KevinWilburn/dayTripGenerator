deca2626e659a696f2471f4b727a9ae99a3bc591
Kevin Wilburn <wilburnkevin200@gmail.com>
2021-05-10 20:30:25 -0400

    Added arrays of what user wants

---
+++ app.js
@@ -0,0 +1,6 @@
+let destination=["Miami","Chicago","Arizona"];
+let restaurant=["Chiptole","Olive Garden", "Buffalo Wild Wings"];
+let transportation=["Plane","Train","Car"];
+let entertainment=["Beach day","Baseball game","Hiking"];
+
+
